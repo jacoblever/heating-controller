065bef1270e9d52b4bfd2c37cc0421c652f0884d
Jacob Lever <jacoblever@gmail.com>
2023-11-11 21:02:44 +0000

    Fix linting errors

--- dashboard/src/Advanced.tsx
+++ dashboard/src/Advanced.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 export function Advanced() {
     const [command, setCommand] = useState<string>("");
--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -32,6 +32,7 @@ function App() {
 
   useEffect(() => {
     updateState(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
 
