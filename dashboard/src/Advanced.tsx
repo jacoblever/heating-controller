602e0a61c52f2f0d8d8e822ccd4dce877df551c3
Jacob Lever <jacoblever@gmail.com>
2025-11-30 12:06:24 +0000

    Make advanced commands easier to use

--- dashboard/src/Advanced.tsx
+++ dashboard/src/Advanced.tsx
@@ -1,20 +1,10 @@
 import React, { useState } from 'react';
 
 export function Advanced() {
-    const [command, setCommand] = useState<string>("");
+    const [customNumber, setCustomNumber] = useState<number>(250);
     const [commandSent, setCommandSent] = useState<boolean>(false);
 
-    function turnBoiler(commandOverride?: string) {
-        var commandToSend = "";
-        if (commandOverride) {
-            commandToSend = commandOverride;
-        } else {
-            commandToSend = command;
-            if (commandToSend === "") {
-                return;
-            }
-        }
-
+    function turnBoiler(commandToSend: string) {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open("GET", "http://192.168.86.100:8080/turn-boiler/?command=" + commandToSend, false);
         xmlHttp.send(null);
@@ -27,24 +17,40 @@ export function Advanced() {
     }
 
     return (<>
-        <div>
-            <label>
-                Turn Boiler (+ve is clockwise):
-                <input
-                    type="text"
-                    value={command}
-                    onChange={(e) => setCommand(e.currentTarget.value)}
-                />
-                <button
-                    onClick={() => turnBoiler()}>
-                    Send
-                </button>
-                {commandSent && (<span>Sent!</span>)}
-            </label>
+        <div style={{ marginBottom: "10px" }}>
+            Turn Boiler (+ve is clockwise, and On direction):
+        </div>
+        <div style={{ marginBottom: "10px" }}>
+            <button
+                onClick={() => turnBoiler(`-${customNumber}`)}>
+                Off (-ve) direction
+            </button>
+            <input
+                type="number"
+                value={customNumber}
+                onChange={(e) => setCustomNumber(+e.currentTarget.value)}
+            />
+            <button
+                onClick={() => turnBoiler(customNumber.toString())}>
+                On (+ve) direction
+            </button>
+        </div>
+        <div style={{ marginBottom: "10px" }}>
+            <button onClick={() => turnBoiler('-250')}>Off 250</button>
+            <button onClick={() => turnBoiler('-100')}>Off 100</button>
+            <button onClick={() => turnBoiler('-50')}>Off 50</button>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button onClick={() => turnBoiler('50')}>On 50</button>
+            <button onClick={() => turnBoiler('100')}>On 100</button>
+            <button onClick={() => turnBoiler('250')}>On 250</button>
+        </div>
+        <div style={{ marginBottom: "10px" }}>
+            <button onClick={() => turnBoiler('turn-anticlockwise')}>Simulate Off</button>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button onClick={() => turnBoiler('turn-clockwise')}>Simulate On</button>
         </div>
         <div>
-            <button onClick={() => turnBoiler('turn-clockwise')}>Turn Clockwise</button>
-            <button onClick={() => turnBoiler('turn-anticlockwise')}>Turn Anticlockwise</button>
+            {commandSent && (<span>Command Sent!</span>)}
         </div>
     </>
     );
